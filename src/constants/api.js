38fac09cda72815a412e6cb253efff2b1584db93
Kenny Tse <tsekennykl1@gmail.com>
2026-08-27 21:50:16 +0800

    change to use JAVA stock retrieval API

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,7 +1,8 @@
 export const API_BASE = "https://z35lnmmzgi.execute-api.ap-east-1.amazonaws.com/prod";
+export const API_BASE_JAVA ="https://s3s78soeq5.execute-api.ap-east-1.amazonaws.com/Prod";
 
 export const ENDPOINTS = {
-  CONSOLIDATED_MONTHLY_REPORT: `${API_BASE}/lambda_consolidated_monthly_report`,
-  STOCK: `${API_BASE}/stock`,
+  CONSOLIDATED_MONTHLY_REPORT: `${API_BASE_JAVA}/report`,
+  STOCK: `${API_BASE}/getStockData`,
   CRUD: `${API_BASE}/lambda_crud_handler`,  // ← Fixed!
 };
